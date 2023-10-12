f5978c55c8fdb2fa4766c960d94c28ef1d4fcb51
shin19970827 <142146779+shin19970827@users.noreply.github.com>
2023-10-13 04:42:23 +0900

    課題提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+let weightValue
+weightValue = 68
+
+let heightValue
+heightValue = 1.7
+
+const bmi = weightValue / (heightValue * heightValue)
+
+console.log('体重' + weightValue + 'kg')
+console.log('身長' + heightValue + 'm');
+console.log('BMI' + bmi)
+
